1d688e86cf94be2ec60ad36c62dbfa337f9d469b
Unknown <kmmk0924kei@gmail.com>
2018-09-25 22:02:33 +0900

    cardreturnメソッドの追加
    選択された２枚のカードが一致しないときに，それらのカードを裏にするcardreturnメソッドの追加．
    cardreturnメソッドの追加に伴い，各メソッドを修正．

--- shinkeisuijaku.js
+++ shinkeisuijaku.js
@@ -1,6 +1,7 @@
-﻿var $card1 = 0;
-var $card2 = 0;
+﻿var $card1 = 0; var $card1id;
+var $card2 = 0; var $card2id;
 var $clickcnt = 0;
+var $timer;
 
 var $cardinfo = [];
 $cardinfo[0] = []; $cardinfo[1] = []; $cardinfo[2] = []; $cardinfo[3] = [];
@@ -12,36 +13,50 @@ $cardinfo[3][0] = 'c02.png'; $cardinfo[3][1] = 2;
 function cardset(n){
  if($clickcnt != 3){
   $(document.getElementById(n.id)).attr("src", $cardinfo[n.id][0]);
-  cardnumset($cardinfo[n.id][1]);
+  cardnumset($cardinfo[n.id][1], n.id);
   $clickcnt++;
 
   if($card1 != 0 && $card2 != 0){
-   result();
+   $timer = setInterval(result, 1000);
   }
  }
 }
 
-function cardnumset(n){
+function cardnumset(n, id){
  if($card1 == 0) {
   $card1 = n;
+  $card1id = id;
  }else{
   $card2 = n;
+  $card2id = id;
  }
 }
 
 function result(){
+ var $result;
+
  if($card1 == $card2){
-  alert('成功');
+  $result = confirm('成功');
  }else{
-  alert('失敗');
+  $result = confirm('失敗');
+  if($result){
+   cardreturn();
+  }
  }
 
+ clearInterval($timer);
  $card1 = 0; $card2 = 0;
 }
 
+function cardreturn(){
+ $(document.getElementById($card1id)).attr("src", $cardinfo[0][0]);
+ $(document.getElementById($card2id)).attr("src", $cardinfo[0][0]);
+ $clickcnt -= 2;
+}
+
 function reset(){
- $("#ura1 > img").attr("src", "z01.png");
- $("#ura2 > img").attr("src", "z01.png");
- $("#ura3 > img").attr("src", "z01.png");
+ $("#ura1 > img").attr("src", $cardinfo[0][0]);
+ $("#ura2 > img").attr("src", $cardinfo[0][0]);
+ $("#ura3 > img").attr("src", $cardinfo[0][0]);
  $card1 = 0; $card2 = 0; $clickcnt = 0;
 }
