c30afcfe405841f72e185202c56507461e786564
Unknown <kmmk0924kei@gmail.com>
2018-10-04 13:57:24 +0900

    カード枚数の増加
    カードを26枚まで追加した．

--- shinkeisuijaku.js
+++ shinkeisuijaku.js
@@ -4,12 +4,20 @@ var $score1 = 0; var $score2 = 0;
 var $player = 1;
 var $clickcnt = 0;
 var $timer;
-var $cardmax = 6;
+var $cardmax = 26;
 
 
 var $cardinfo = [];
-$cardinfo[0] = []; $cardinfo[1] = []; $cardinfo[2] = []; $cardinfo[3] = [];
-$cardinfo[4] = []; $cardinfo[5] = []; $cardinfo[6] = [];
+$cardinfo[0] = [];$cardinfo[1] = []; $cardinfo[2] = [];
+$cardinfo[3] = [];$cardinfo[4] = []; $cardinfo[5] = [];
+$cardinfo[6] = [];$cardinfo[7] = []; $cardinfo[8] = [];
+$cardinfo[9] = [];$cardinfo[10] = []; $cardinfo[11] = [];
+$cardinfo[12] = [];$cardinfo[13] = []; $cardinfo[14] = [];
+$cardinfo[15] = [];$cardinfo[16] = []; $cardinfo[17] = [];
+$cardinfo[18] = [];$cardinfo[19] = []; $cardinfo[20] = [];
+$cardinfo[21] = [];$cardinfo[22] = []; $cardinfo[23] = [];
+$cardinfo[24] = [];$cardinfo[25] = []; $cardinfo[26] = [];
+
 $cardinfo[0][0] = 'z01.png'; $cardinfo[0][1] = 0;
 $cardinfo[1][0] = 'c01.png'; $cardinfo[1][1] = 1;
 $cardinfo[2][0] = 'c01.png'; $cardinfo[2][1] = 1;
@@ -17,16 +25,47 @@ $cardinfo[3][0] = 'c02.png'; $cardinfo[3][1] = 2;
 $cardinfo[4][0] = 'c02.png'; $cardinfo[4][1] = 2;
 $cardinfo[5][0] = 'c03.png'; $cardinfo[5][1] = 3;
 $cardinfo[6][0] = 'c03.png'; $cardinfo[6][1] = 3;
+$cardinfo[7][0] = 'c04.png'; $cardinfo[7][1] = 4
+$cardinfo[8][0] = 'c04.png'; $cardinfo[8][1] = 4;
+$cardinfo[9][0] = 'c05.png'; $cardinfo[9][1] = 5;
+$cardinfo[10][0] = 'c05.png'; $cardinfo[10][1] = 5;
+$cardinfo[11][0] = 'c06.png'; $cardinfo[11][1] = 6;
+$cardinfo[12][0] = 'c06.png'; $cardinfo[12][1] = 6;
+$cardinfo[13][0] = 'c07.png'; $cardinfo[13][1] = 7;
+$cardinfo[14][0] = 'c07.png'; $cardinfo[14][1] = 7;
+$cardinfo[15][0] = 'c08.png'; $cardinfo[15][1] = 8;
+$cardinfo[16][0] = 'c08.png'; $cardinfo[16][1] = 8;
+$cardinfo[17][0] = 'c09.png'; $cardinfo[17][1] = 9;
+$cardinfo[18][0] = 'c09.png'; $cardinfo[18][1] = 9;
+$cardinfo[19][0] = 'c10.png'; $cardinfo[19][1] = 10;
+$cardinfo[20][0] = 'c10.png'; $cardinfo[20][1] = 10;
+$cardinfo[21][0] = 'c11.png'; $cardinfo[21][1] = 11;
+$cardinfo[22][0] = 'c11.png'; $cardinfo[22][1] = 11;
+$cardinfo[23][0] = 'c12.png'; $cardinfo[23][1] = 12;
+$cardinfo[24][0] = 'c12.png'; $cardinfo[24][1] = 12;
+$cardinfo[25][0] = 'c13.png'; $cardinfo[25][1] = 13;
+$cardinfo[26][0] = 'c13.png'; $cardinfo[26][1] = 13;
 
 var $setcard = [];
-$setcard[0] = []; $setcard[1] = []; $setcard[2] = []; $setcard[3] = [];
-$setcard[4] = []; $setcard[5] = []; $setcard[6] = [];
+$setcard[0] = [];$setcard[1] = []; $setcard[2] = [];
+$setcard[3] = [];$setcard[4] = []; $setcard[5] = [];
+$setcard[6] = [];$setcard[7] = []; $setcard[8] = [];
+$setcard[9] = [];$setcard[10] = []; $setcard[11] = [];
+$setcard[12] = [];$setcard[13] = []; $setcard[14] = [];
+$setcard[15] = [];$setcard[16] = []; $setcard[17] = [];
+$setcard[18] = [];$setcard[19] = []; $setcard[20] = [];
+$setcard[21] = [];$setcard[22] = []; $setcard[23] = [];
+$setcard[24] = [];$setcard[25] = []; $setcard[26] = [];
 $setcard[0][0] = 'z01.png'; $setcard[0][1] = 0;
 
 window.onload = function shuffle(){
- var $arr = [1, 2, 3, 4, 5, 6];
- var $a = $arr.length;
+ var $arr = [];
 
+ for(var i = 0; i < 26; i++){
+  $arr[i] = i + 1;
+ }
+
+ $a = $arr.length;
  while($a){
   var j = Math.floor(Math.random() * $a);
   var t = $arr[--$a];
